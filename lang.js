ee22108f1888702e344baa617e0c6215b831ec2d
Fek1ra <103352849+Fek1ra@users.noreply.github.com>
2022-11-12 14:34:48 +0200

    ver 1.0

---
+++ lang.js
@@ -0,0 +1,267 @@
+const langArr = {
+//title 	
+	"unit" : {
+			"ua" : "Hide - найзручніший месенджер",
+			"ru" : "Hide - самый удобный мессенджер",
+			"en" : "Hide - the most comfortable massanger",
+	},
+
+//nav-bar	
+
+	"list1" : { 
+		"ua" : "Головна",
+		"ru" : "Главная",
+		"en" : "Main",
+	},
+	"list2" : {
+		"ua" : "Про нас",
+		"ru" : "О нас",
+		"en" : "About us",
+	},
+	"list3" : {
+		"ua" : "Ціна",
+		"ru" : "Цена",
+		"en" : "Price",
+	},
+	"list4" : {
+		"ua" : "Контакти",
+		"ru" : "Контакты",
+		"en" : "Contacts",
+	},
+	"reg" : {
+		"ua" : "Зареєструватися",
+		"ru" : "Зарегестрироваться",
+		"en" : "Register",
+	},
+	"auth" : {
+		"ua" : "Авторизуватся",
+		"ru" : "Авторизироваься",
+		"en" : "Authorization",
+	},
+
+//first-sidebar-left
+
+	"sb-text" : {
+		"ua" : "Найзручніший ігровий месенджер",
+		"ru" : "Самый удобный игровой мессенджер",
+		"en" : "The most convenient gaming messenger",
+	},
+	"sb-text-second" : {
+		"ua" : "Якщо ти вже давно хотів спробувати новий ігровий месенджер, то"
+		+ " " + "Hide не стане розчаруванням. Він позбавить тебе від незручних"
+		+ " " + "перемикань між різними засобами спілкування і запропунує тобі"
+		+ " " + "багато корисних і приємних функцій. Спробуй перші 15 днів безкоштовно!",
+		"ru" : "Если ты уже давно хотел попробовать новый игровой мессенджер, то"
+		+ " " + "Hide не станет разочарованием. Он избавит тебя от неудобных"
+		+ " " + "переключений между разными средствами общения и предоставит тебе"
+		+ " " + "много полезных и приятных функций. Попробуй первые 15 дней бесплатно!",
+		"en" : "If you have been wanting to try a new game messenger for a long time, then"
+		+ " " + "Hide will not be a disappointment. It will save you from inconvenient switching "
+		+ " " + "between different means of communication and provide you with many useful and"
+		+ " " + "enjoyable features. Try the first 15 days for free!",
+	},
+		"sb-button" : {
+		"ua" : "Спробуй безкоштовно",
+		"ru" : "Попробовать бесплатно",
+		"en" : "Try free",
+	},
+
+//interblock-structure (1)
+
+		"community" : {
+		"ua" : "Приєднуйся до нашої спільноти!",
+		"ru" : "Присооденяйся к нашему сообщества!",
+		"en" : "Join to our community",
+	},
+
+//second-sidebar-right
+
+		"downloads" : {
+		"ua" : "Завантажень",
+		"ru" : "Скачиваний",
+		"en" : "Downloads",
+	},
+		"positive-review" : {
+		"ua" : "Позитивних відгуків",
+		"ru" : "Положительных отзывов",
+		"en" : "Positive reviews",
+	},
+		"active-users" : {
+		"ua" : "Активних користувачів",
+		"ru" : "Активных пользователей",
+		"en" : "Active users",
+	},
+		"customization-options" : {
+		"ua" : "Можливості для персоналізації",
+		"ru" : "Возможностей для персонализации",
+		"en" : "Customization options",
+	},
+		"presets" : {
+		"ua" : "Унікальних пресетів тем і звукових доповнень",
+		"ru" : "Уникальных пресетов тем і звуковых дополнений",
+		"en" : "Unique theme presets and sound additions",
+	},
+
+//interblock-structure (2)
+
+		"second-interblock-structure" : {
+		"ua" : "Чому саме наш месенджер",
+		"ru" : "Почему именно наш мессенджер",
+		"en" : "Why choose our messenger",
+	},
+
+//container-benefit
+//first-benefit-block
+
+		"optimization" : {
+		"ua" : "Оптимізація",
+		"ru" : "Оптимизация",
+		"en" : "Optimization",
+	},
+		"first-benefit-block" : {
+		"ua" : "Hide адаптований і розрахований для користувачів з"
+		+ " " + "невимогливими девайсами. І дозволяє насолоджуватися всіма"
+		+ " " + "функціями застосунку без лагів. ",
+		"ru" : "Hide адаптирован и рассчитан для пользователей с"
+		+ " " + "нетребовательними девайсами. И позволяет наслаждаться всеми"
+		+ " " + "функциями приложения без лагов. ",
+		"en" : "Hide is adapted and designed for users with"
+		+ " " + "undemanding devices. And allows you to enjoy all the"
+		+ " " + "features of the application without lags.",
+	},
+
+//second-benefit-block
+
+		"personalisation" : {
+		"ua" : "Персоналізація",
+		"ru" : "Персонализация",
+		"en" : "Personalisation",
+	},
+		"second-benefit-block" : {
+		"ua" : "Завдяки гнучким налаштуванням під користувача,"
+		+ " " + "Hide надає велику кількість можливостей для"
+		+ " " + "персоналізації всередині застосунку. ",
+		"ru" : "Благодаря гибкой настройке под пользователя,"
+		+ " " + "Hide предоставляет большое количество возможностей"
+		+ " " + "для персонализации внутри приложения. ",
+		"en" : "Thanks to flexible customization for the user, Hide"
+		+ " " + "provides a large number of options"
+		+ " " + "for personalization within the application.",
+	},
+
+//third-benefit-block
+
+		"convenience" : {
+		"ua" : "Зручність",
+		"ru" : "Удобство",
+		"en" : "Convenience",
+	},
+		"third-benefit-block" : {
+		"ua" : "Месенджер Hide повністю позбавляє вас від"
+		+ " " + "проблем з перемиканням між застосунками, в "
+		+ " " + "пошуку найзручніших функцій у кожному з них."
+		+ " " + "Hide має всі зручності в одному застосунку. ",
+		"ru" : "Мессенджер Hide полностью избавляет вас от"
+		+ " " + "проблем с переключением между приложениями,"
+		+ " " + "в поиске самых удобных функций в каждом из них."
+		+ " " + "Hide имеет все удобства в одном приложении. ",
+		"en" : "Messenger Hide completely relieves you of problems"
+		+ " " + "with switching between applications"
+		+ " " + "in search of the most convenient functions in each of them."
+		+ " " + "Hide has all the conveniences in one app.",
+	},
+
+//fourth-benefit-block
+
+		"integration" : {
+		"ua" : "Інтегрованність",
+		"ru" : "Интегрированность",
+		"en" : "Integration",
+	},
+		"fourth-benefit-block" : {
+		"ua" : "З внутрішньоігровим оверлеєм Hide ви не втрачаєте"
+		+ " " + "можливість отримувати інформацію про розмову"
+		+ " " + "і повідомленнях прямо всередині гри. ",
+		"ru" : "С внутриигровым оверлеем Hide вы не теряете"
+		+ " " + "возможность получать информацию о разговоре"
+		+ " " + "и сообщениях прямо внутри игры. ",
+		"en" : "With the Hide in-game overlay, you don't lose the ability to"
+		+ " " + "receive conversation and message information right "
+		+ " " + "inside the game. ",
+	},
+
+//fifth-benefit-block
+
+		"security" : {
+		"ua" : "Безпека",
+		"ru" : "Безопасность",
+		"en" : "Security",
+	},
+		"fifth-benefit-block" : {
+		"ua" : "Про ваші дані можете не безпокоїтися, "
+		+ " " + "адже Hide має власне шифрування повідомлень та розмов. "
+		+ " " + "З Hide можете не переживати про витік ваших данних. ",
+		"ru" : "Про ваши данные можно не беспокоиться, ведь Hide"
+		+ " " + "имеет собственное шифрование сообщений и разговоров. "
+		+ " " + "С Hide можете не переживать о утечке ваших данных. ",
+		"en" : "You don't have to worry about your data, because Hide"
+		+ " " + "has its own encryption of messages and conversations. "
+		+ " " + "With Hide, you don't have to worry about your data being leaked. ",
+	},
+
+//sixth-benefit-block
+
+		"modernity" : {
+		"ua" : "Сучасність",
+		"ru" : "Современность",
+		"en" : "Modernity",
+	},
+		"sixth-benefit-block" : {
+		"ua" : "Hide оснащений найсучаснішими технологіями, "
+		+ " " + "котрі не поступаються конкурентам, а в деяких випадках"
+		+ " " + "навіть відриваються вперед. Чого тільки коштує розширена"
+		+ " " + "демонстрація екрану і обмін даними напряму без серверів. ",
+		"ru" : "Hide оснащён самыми современными технологиями, "
+		+ " " + "которые не уступают конкурентам, а в некоторых"
+		+ " " + "случаях даже отрываються вперед. Чего только стоит расширенная"
+		+ " " + "демонстрация экрана и обмен данными напрямую без серверов. ",
+		"en" : "Hide is equipped with the latest technology that is not"
+		+ " " + "inferior to competitors, and in some cases even come off"
+		+ " " + "ahead. What is only an extended screen sharing and data"
+		+ " " + "exchange directly without servers. ",
+	},
+
+//third-sidebar-left
+
+		"sb-text-third" : {
+		"ua" : "Грай в найвимогливіші ігри через комп'ютер свого друга",
+		"ru" : "Играй в самые требовательные игры через компьютер своего друга",
+		"en" : "Play the most demanding games on your friend's computer",
+	},
+		"sb-text-third-first-block" : {
+		"ua" : "Hide представляє функцію Advanced Screen Sharing"
+		+ " " + "З її допомогою  користувачі, окрім доступа до перегляду екрану"
+		+ " " + "отримають можливість управління комп'ютером співрозмовника. ",
+		"ru" : "Hide представляет функцию Advanced Screen Sharing. "
+		+ " " + "С ее помощью пользователи, помимо доступа к просмотру экрана"
+		+ " " + "получат возможность управления компьютером собеседника. ",
+		"en" : "Hide introduces Advanced Screen Sharing. "
+		+ " " + "With its help, users, in addition to access to viewing the"
+		+ " " + "screen, will be able to control the interlocutor's computer. ",
+	},
+		"sb-text-third-second-block" : {
+		"ua" : "Ця функція відкриває безліч можливостей для різних видів"
+		+ " " + "користувачів. Від гри на слабій конфігурації комп'ятера в найвимогливіші"
+		+ " " + "ігри без затримок та лагів, до зручної роботи розробників, котрі"
+		+ " " + "зможуть дистанційно давати доступ до свого проекту",
+		"ru" : "Эта функция открывает множество возможностей для разных видов"
+		+ " " + "пользователей. От игры на слабой конфигурации компьютера в самые"
+		+ " " + "требовательные игры  без задержек и лагов, до удобной работы разработчиков, "
+		+ " " + "которые смогут дистанционно давать доступ к своему проекту. ",
+		"en" : "This feature opens up many possibilities for different"
+		+ " " + "kinds of users. From playing the most demanding games"
+		+ " " + "on a weak computer configuration without delays and lags, to the"
+		+ " " + "convenient work of developers who can remotely give access to their project. ",
+	},
+
+}
